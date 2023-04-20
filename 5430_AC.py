38f1843ebe12590bb3b86c8798ccad394ee29884
nastorond <nastorond@kakao.com>
2023-04-20 13:13:06 +0900

    2023_4_20

--- 5430_AC.py
+++ 5430_AC.py
@@ -2,19 +2,24 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 out=sys.stdout.write
-test=int(input())
-for _ in range(test):
+test_case=int(input())
+for _ in range(test_case):
     p=deque(input().rstrip())
     n=int(input())
-    s=input().rstrip()
-    li=deque()
-    for i in s:
-        if i.isnumeric():li.append(i)
+    li=deque(input().rstrip()[1:-1].split(','))
+    ##아무것도 없어도 len이 1로 잡힘, 입력 저렇게도 가능
+    cnt,flag=0,0
+    if n==0:li=[]
     for j in p:
         if j=='R':
-            li.reverse()
+            cnt+=1
         elif j=='D':
-            if len(li)>1:li.popleft()
-            else:out('error\n');break
-    if len(li)>1:
-        out('['+','.join(li)+']\n')
+            if len(li)<1:flag=1;out('error\n');break
+            else:
+                if cnt%2==0:li.popleft()
+                else:li.pop()
+    if flag==0:
+        if cnt%2==0:out('['+','.join(li)+']\n')
+        else:
+            li.reverse()
+            out('['+','.join(li)+']\n')
