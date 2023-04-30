519869fc3f5a2e8543dc6151d497987168611acc
nastorond <nastorond@kakao.com>
2023-05-01 01:16:33 +0900

    2023_5_1

--- baekjoon.py
+++ baekjoon.py
@@ -1819,30 +1819,34 @@ print(*ans_dic)
 # print(ans)
 
 ## 24479 알고리즘 수업 - 깊이 우선 탐색 1
-import sys
-sys.setrecursionlimit(10 ** 6)
-input=sys.stdin.readline
-n,m,r=map(int,input().split())
-gp=[[] for _ in range(n+1)]
-vt=[0]*(n+1)
-cnt=1
+# import sys
+# sys.setrecursionlimit(10 ** 6)
+# input=sys.stdin.readline
+# n,m,r=map(int,input().split())
+# gp=[[] for _ in range(n+1)]
+# vt=[0]*(n+1)
+# cnt=1
 
-def dfs(gp,e,vt):
-    global cnt
-    vt[e]=cnt
-    for i in gp[e]:
-        if vt[i]==0:
-            cnt+=1
-            dfs(gp,i,vt)
+# def dfs(gp,e,vt):
+#     global cnt
+#     vt[e]=cnt
+#     for i in gp[e]:
+#         if vt[i]==0:
+#             cnt+=1
+#             dfs(gp,i,vt)
 
-for _ in range(m):
-    i,j=map(int,input().split())
-    gp[i].append(j)
-    gp[j].append(i)
-for i in range(n):gp[i].sort()
-dfs(gp,r,vt)
-for i in range(1,n+1):print(vt[i])
+# for _ in range(m):
+#     i,j=map(int,input().split())
+#     gp[i].append(j)
+#     gp[j].append(i)
+# for i in range(n):gp[i].sort()
+# dfs(gp,r,vt)
+# for i in range(1,n+1):print(vt[i])
 
+## 2752 세수 정렬
+from collections import deque
+li=deque(map(int,input().split()))
+print(*sorted(li))
 
 
 
