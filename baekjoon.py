25474bbc803b0360ef1143dc64101204196e0ebc
nas <nastorond@kakao.com>
2023-05-12 22:34:46 +0900

    2023_5_12

--- baekjoon.py
+++ baekjoon.py
@@ -1990,18 +1990,25 @@ print(*ans_dic)
 # print(cnt)
 
 ## 15829 Hashing
-import sys
-input=sys.stdin.readline
-dc={'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 
-    'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17,
-    'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
-n=int(input())
-s=input().rstrip()
-res=0
-for i in range(len(s)):
-    res+=dc[s[i]]*(31**i)
-print(res%1234567891)
+# import sys
+# input=sys.stdin.readline
+# dc={'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 
+#     'j':10, 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 'p':16, 'q':17,
+#     'r':18, 's':19, 't':20, 'u':21, 'v':22, 'w':23, 'x':24, 'y':25, 'z':26}
+# n=int(input())
+# s=input().rstrip()
+# res=0
+# for i in range(len(s)):
+#     res+=dc[s[i]]*(31**i)
+# print(res%1234567891)
 
+## 10039 평균 점수
+res=0
+for i in range(5):
+    n=int(input())
+    if n<40:n=40
+    res+=n
+print(res//5)
 
 
 
