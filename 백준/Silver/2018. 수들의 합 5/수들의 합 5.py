7bdbae17a49ff0a661cd2d9d8ef2e4ccfe56ded1
nastorond <nastorond@kakao.com>
2023-08-13 13:46:18 +0900

    update answer

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -1,3 +1,26 @@
+# import sys
+
+# input = sys.stdin.readline
+
+# n = int(input())
+# start = 1
+# end = 2
+# cnt = 0
+# num_list = list(range(n+1))
+# while start <= end:
+#     ck = 0
+#     # for i in range(start, end+1):
+#     for i in num_list[start:end+1]:
+#         ck += i
+#     if ck == n:
+#         cnt += 1
+#         start += 1
+#     elif ck < n:
+#         end += 1
+#     else:
+#         start += 1
+# print(cnt)
+
 n = int(input())
 cnt, ck = 0, 0
 start, end = 0, 0
