2eb06c3560dceb9ffd1db7e6a209444e9c10d765
nastorond <86664097+nastorond@users.noreply.github.com>
2023-08-24 22:06:13 +0900

    [Silver II] Title: 사탕 게임, Time: 140 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -7,11 +7,16 @@ n = int(input())
 candies = [list(input().rstrip()) for _ in range(n)]
 
 
+# 사탕의 연속한 수를 세는 함수
 def func(x, y, u, v):
+    # 아래 for 문을 1부터 시작할 거라서
     cnt1, cnt2 = 1, 1
+    # 카운트를 모아줄 리스트
     f_cnt = []
+    # 사탕의 위치를 바꾼다
     candies[x][y], candies[u][v] = candies[u][v], candies[x][y]
     for add_idx in range(1, n):
+        # 연속된 사탕의 개수를 세어준다
         if candies[add_idx - 1][y] == candies[add_idx][y]:
             cnt1 += 1
         else:
@@ -22,8 +27,8 @@ def func(x, y, u, v):
         else:
             f_cnt.append(cnt2)
             cnt2 = 1
-        f_cnt.append(cnt1)
-        f_cnt.append(cnt2)
+    f_cnt.append(cnt1)
+    f_cnt.append(cnt2)
 
     candies[x][y], candies[u][v] = candies[u][v], candies[x][y]
 
