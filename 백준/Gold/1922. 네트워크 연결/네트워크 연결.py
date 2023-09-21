3aa15353489e46cc91b9cef8e450ff1aff5e62ec
nastorond <86664097+nastorond@users.noreply.github.com>
2023-09-21 22:16:22 +0900

    [Gold IV] Title: 네트워크 연결, Time: 288 ms, Memory: 51012 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+def find_set(x):
+    if parents[x] == x:
+        return x
+    parents[x] = find_set(parents[x])
+    return parents[x]
+def union(x, y):
+    x = find_set(x)
+    y = find_set(y)
+    if x == y:
+        return
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+V = int(input())
+E = int(input())
+edges = []
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edges.append([u, v, w])
+edges.sort(key=lambda x: x[2])
+parents = [i for i in range(V + 1)]
+cnt = 0
+weights = 0
+for u, v, w in edges:
+    if find_set(u) != find_set(v):
+        cnt += 1
+        weights += w
+        union(u, v)
+
+        if cnt == V:
+            break
+print(weights)
