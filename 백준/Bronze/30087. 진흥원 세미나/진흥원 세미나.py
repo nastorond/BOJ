b81592e585729e63f80f2a4ba222e660a6be95a6
nastorond <86664097+nastorond@users.noreply.github.com>
2023-10-17 22:01:10 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,4 @@
+answer_sheet = {'Algorithm':204, 'DataAnalysis':'207', 'ArtificialIntelligence':'302',
+               'CyberSecurity':'B101', 'Network':'303', 'Startup':'501', 'TestStrategy':'105'}
+for _ in range(int(input())):
+    print(answer_sheet[input()])
