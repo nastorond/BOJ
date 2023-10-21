5d758bc1a2fd43e8627acacee9d91b1b0e2270ed
nastorond <86664097+nastorond@users.noreply.github.com>
2023-10-22 00:02:47 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    if (N==0){
+        cout << "YONSEI\n";
+    }else{
+        cout << "Leading the Way to the Future\n";
+    }
+    return 0;
+}
