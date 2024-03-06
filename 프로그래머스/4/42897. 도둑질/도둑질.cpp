b1b3ab417fdc269d4f78268b7e2fd21b96133def
nastorond <86664097+nastorond@users.noreply.github.com>
2024-03-06 17:52:28 +0900

    [level 4] Title: 도둑질, Time: 11.68 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dp1[1000000 + 30];
+int dp2[1000000 + 30];
+
+int solution(vector<int> sticker)
+{
+    int answer = 0;
+    
+    dp1[0] = sticker[0];
+    dp1[1] = sticker[0];
+    
+    dp2[0] = 0;
+    dp2[1] = sticker[1];
+    
+    for (int i=2; i<sticker.size()-1; i++) {
+        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1]);
+    }
+    
+    for (int i=2; i<sticker.size(); i++) {
+        dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1]);
+    }
+    
+    answer = *max(max_element(dp1, dp1+sticker.size()), max_element(dp1, dp2+sticker.size()));
+    
+    return answer;
+}
