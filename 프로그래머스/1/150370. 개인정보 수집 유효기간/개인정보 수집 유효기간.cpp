8d30348e90ce4a2e3cf97bab96c9b17da89dc576
nastorond <86664097+nastorond@users.noreply.github.com>
2024-08-16 15:52:48 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.07 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -10,78 +10,69 @@ struct info {
     int month;
     int day;
 };
-vector<int> todays_info;
-vector<info> custom_info;
+
 unordered_map<char, int> option;
 
-void cal_custom_info (vector<string>& privacies) {
-    for (int i=0; i<privacies.size(); i++) {
+vector<int> parse_date(const string& date) {
+    vector<int> date_info(3);
+    date_info[0] = stoi(date.substr(0, 4));
+    date_info[1] = stoi(date.substr(5, 2));
+    date_info[2] = stoi(date.substr(8, 2));
+    return date_info;
+}
+
+void cal_custom_info(vector<string>& privacies, vector<info>& custom_info) {
+    custom_info.reserve(privacies.size());
+    
+    for (int i = 0; i < privacies.size(); i++) {
         info tmp_info;
-        tmp_info.num = i+1;
+        tmp_info.num = i + 1;
         tmp_info.year = stoi(privacies[i].substr(0, 4));
         tmp_info.month = stoi(privacies[i].substr(5, 2)) + option[privacies[i].back()];
         tmp_info.day = stoi(privacies[i].substr(8, 2)) - 1;
         
+        if (tmp_info.day == 0) {
+            tmp_info.month--;
+            if (tmp_info.month == 0) {
+                tmp_info.month = 12;
+                tmp_info.year--;
+            }
+            tmp_info.day = 28;
+        }
+
         while (tmp_info.month > 12) {
             tmp_info.year++;
             tmp_info.month -= 12;
-        }
-        
-        if (tmp_info.day == 0) {
-            if (tmp_info.month == 1) {
-                tmp_info.month = 13;
-                tmp_info.year--;
-            }
-            tmp_info.month--;
-            tmp_info.day = 28;
         }
         
         custom_info.push_back(tmp_info);
     }
 }
 
-vector<int> ck_terms () {
+vector<int> ck_terms(const vector<int>& todays_info, const vector<info>& custom_info) {
     vector<int> res;
-    for (int i=0; i<custom_info.size(); i++) {
-        info cus = custom_info[i];
-        if (cus.year > todays_info[0]) continue;
-        else if (cus.year < todays_info[0]) {
-            res.push_back(cus.num); continue;
-        } 
-        else {
-            if (cus.month > todays_info[1]) continue;
-            else if (cus.month < todays_info[1]) {
-                res.push_back(cus.num); continue;
-            }
-            else {
-                if (cus.day > todays_info[2]) continue;
-                else if (cus.day < todays_info[2]) {
-                    res.push_back(cus.num);
-                }
-            }
+    res.reserve(custom_info.size());
+    
+    for (const auto& cus : custom_info) {
+        if (cus.year < todays_info[0] || 
+            (cus.year == todays_info[0] && cus.month < todays_info[1]) || 
+            (cus.year == todays_info[0] && cus.month == todays_info[1] && cus.day < todays_info[2])) {
+            res.push_back(cus.num);
         }
     }
+    
     return res;
 }
 
 vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
-    vector<int> answer;
+    vector<int> todays_info = parse_date(today);
+    vector<info> custom_info;
     
-    string tmp = "";
-    for (char c: today) {
-        if (c != '.') {
-            tmp += c; continue;
-        }
-        todays_info.push_back(stoi(tmp));
-        tmp = "";
-    }
-    todays_info.push_back(stoi(tmp));
-    
-    for (int i=0; i<terms.size(); i++) {
-        option[terms[i][0]] = stoi(terms[i].substr(2));
+    for (const auto& term : terms) {
+        option[term[0]] = stoi(term.substr(2));
     }
     
-    cal_custom_info(privacies);
+    cal_custom_info(privacies, custom_info);
     
-    return ck_terms();
+    return ck_terms(todays_info, custom_info);
 }
