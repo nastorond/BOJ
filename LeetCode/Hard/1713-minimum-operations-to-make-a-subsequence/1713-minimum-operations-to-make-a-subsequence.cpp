fb7ff129495c1ceee8046adc314c071f6258e966
nastorond <86664097+nastorond@users.noreply.github.com>
2024-08-20 21:27:53 +0900

    Time: 224 ms (60.06%), Space: 113.8 MB (79.26%) - LeetHub

---
+++ LeetCode/Hard/1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& target, vector<int>& arr) {
+        unordered_map<int, int> hashing;
+        vector<int> tmp;
+        int lim=target.size();
+        
+        for (int i=0; i<lim; i++) hashing[target[i]] = i;
+        
+        for (int i : arr) {
+            if (hashing.find(i) == hashing.end()) continue;
+            
+            auto it = lower_bound(tmp.begin(), tmp.end(), hashing[i]);
+            
+            if (it == tmp.end()) {
+                tmp.push_back(hashing[i]);
+            } else {
+                *it = hashing[i];
+            }
+        }
+        
+        return lim - tmp.size();
+    }
+};
