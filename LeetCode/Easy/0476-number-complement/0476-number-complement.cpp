060adb91e4644fe0cd935e4f139b22c53138da51
nastorond <86664097+nastorond@users.noreply.github.com>
2024-08-22 12:41:55 +0900

    Time: 0 ms (100.00%), Space: 7.5 MB (57.13%) - LeetHub

--- LeetCode/Easy/0476-number-complement/0476-number-complement.cpp
+++ LeetCode/Easy/0476-number-complement/0476-number-complement.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     unsigned int findComplement(unsigned int num) {
         
-        if (num == 1) return 0;
+        unsigned int mask = ~0;
         
-        unsigned int bit = log2(num) + 1;
-        unsigned int mask = (1U << bit) - 1;
+        while (num & mask) {
+            mask <<= 1;
+        }
         
-        return num^mask;
+        return ~mask & ~num;
     }
 };
