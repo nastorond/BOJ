86f3a26a85842331f7dbeb07b63604d2b185264d
nastorond <86664097+nastorond@users.noreply.github.com>
2024-08-23 20:20:34 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool visited[52] = {false, };
+int answer = 1e9;
+
+bool canSwitch(string cur, string next) {
+    int cnt = 0;
+    for (int i=0; i<cur.size(); i++) {
+        if (cur[i] != next[i]) cnt++;
+    }
+    return cnt == 1 ? true : false;
+}
+
+void dfs (int cur, int turn, string goal, vector<string> words) {
+    if (goal == words[cur]) {
+        answer = min(answer, turn);
+        return ;
+    }
+    for (int i=0; i<words.size()-1; i++) {
+        if (i != cur && !visited[i] && canSwitch(words[cur], words[i])) {
+            visited[i] = true;
+            dfs(i, turn+1, goal, words);
+            visited[i] = false;
+        }
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+    words.push_back(begin);
+    if (find(words.begin(), words.end(), target) == words.end()) return 0;
+    
+    dfs(words.size()-1, 0, target, words);
+    
+    return answer;
+}
