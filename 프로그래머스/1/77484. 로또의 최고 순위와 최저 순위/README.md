f11eca01f4574a5399588885f88813b544805568
nastorond <86664097+nastorond@users.noreply.github.com>
2024-08-29 22:00:16 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#fn1) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.02 ms
+메모리: 4.23 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 29일 21:39:48
+2024년 08월 29일 22:00:15
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -1,64 +1,24 @@
 #include <string>
 #include <vector>
-#include <cstring>
 #include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
-    vector<int> answer;
-    
-    int checkNum[6], lim = lottos.size(), score=0, cnt=0;
-    memset(checkNum, -1, sizeof(checkNum));
-    
-    for (int i=0; i<lim; i++) {
-        if (lottos[i] == 0) {
-            cnt++;
-            continue;
-        }
-        if (find(win_nums.begin(), win_nums.end(), lottos[i]) != win_nums.end()) {
-            checkNum[i] = 1;
-            score++;
-        } else {
-            checkNum[i] = 0;
+    int cnt_zeros = count(lottos.begin(), lottos.end(), 0);
+    int cnt_matches = 0;
+
+    for (int num : lottos) {
+        if (find(win_nums.begin(), win_nums.end(), num) != win_nums.end()) {
+            cnt_matches++;
         }
     }
     
-    if (score + cnt == 6) {
-        answer.push_back(1);
-    } else if (score + cnt == 5) {
-        answer.push_back(2);
-    } else if (score + cnt == 4) {
-        answer.push_back(3);
-    } else if (score + cnt == 3) {
-        answer.push_back(4);
-    } else if (score + cnt == 2) {
-        answer.push_back(5);
-    } else {
-        answer.push_back(6);
-    }
-    
-    switch (score) {
-        case 6:
-            answer.push_back(1);
-            break;
-        case 5:
-            answer.push_back(2);
-            break;
-        case 4:
-            answer.push_back(3);
-            break;
-        case 3:
-            answer.push_back(4);
-            break;
-        case 2:
-            answer.push_back(5);
-            break;
-        default:
-            answer.push_back(6);
-            break;
-    }
-        
-    
-    return answer;
+    int max_rank = 7 - (cnt_matches + cnt_zeros);
+    int min_rank = 7 - cnt_matches;
+
+    if (max_rank > 6) max_rank = 6;
+    if (min_rank > 6) min_rank = 6;
+
+    return {max_rank, min_rank};
 }
