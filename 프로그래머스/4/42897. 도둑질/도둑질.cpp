2b3fcd8bf77a7eb34f8c49bdc29d16ea8c588f6f
nastorond <86664097+nastorond@users.noreply.github.com>
2024-08-31 14:09:31 +0900

    [level 4] Title: 도둑질, Time: 11.97 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -7,25 +7,25 @@ using namespace std;
 int dp1[1000000 + 30];
 int dp2[1000000 + 30];
 
-int solution(vector<int> sticker)
+int solution(vector<int> money)
 {
     int answer = 0;
     
-    dp1[0] = sticker[0];
-    dp1[1] = sticker[0];
+    dp1[0] = money[0];
+    dp1[1] = money[0];
     
     dp2[0] = 0;
-    dp2[1] = sticker[1];
+    dp2[1] = money[1];
     
-    for (int i=2; i<sticker.size()-1; i++) {
-        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1]);
+    for (int i=2; i<money.size()-1; i++) {
+        dp1[i] = max(dp1[i-2] + money[i], dp1[i-1]);
     }
     
-    for (int i=2; i<sticker.size(); i++) {
-        dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1]);
+    for (int i=2; i<money.size(); i++) {
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1]);
     }
     
-    answer = *max(max_element(dp1, dp1+sticker.size()), max_element(dp1, dp2+sticker.size()));
+    answer = *max(max_element(dp1, dp1+money.size()), max_element(dp1, dp2+money.size()));
     
     return answer;
 }
