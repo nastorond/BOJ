3184c57d17eafe50ed450105aa4c826707312a19
nastorond <86664097+nastorond@users.noreply.github.com>
2024-09-12 20:26:11 +0900

    Time: 52 ms (33.94%), Space: 34 MB (58.62%) - LeetHub

--- LeetCode/Easy/1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ LeetCode/Easy/1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int countConsistentStrings(string allowed, vector<string>& words) {
-        unordered_set<char> hashing;
+    int countConsistentStrings(string allowed, vector<string>& words) {      
+        bool arr[26] = {false, };
         int res = 0;
         
         for (char c: allowed) {
-            hashing.insert(c);    
+            arr[c-'a'] = true;  
         }
         
         for (string s : words) {
             int cnt=0;
             for (char c: s) {
-                if (hashing.count(c)) cnt++;
+                if (arr[c-'a']) cnt++;
             }
             if (cnt == s.size()) res++;
         }
