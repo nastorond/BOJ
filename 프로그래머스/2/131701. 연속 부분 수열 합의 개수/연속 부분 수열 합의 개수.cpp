07ca00d277a07ad9c7950d91463d4d5237d98b36
nastorond <86664097+nastorond@users.noreply.github.com>
2024-09-22 22:33:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 76.28 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -11,15 +11,17 @@ int solution(vector<int> elements) {
     // elements 배열을 두 배로 확장하여 원형 배열을 처리
     vector<int> extendedElements = elements;
     extendedElements.insert(extendedElements.end(), elements.begin(), elements.end());
+
+    // 누적 합 배열 생성
+    vector<int> prefixSum(2 * len + 1, 0);
+    for (int i = 1; i <= 2 * len; i++) {
+        prefixSum[i] = prefixSum[i - 1] + extendedElements[i - 1];
+    }
     
-    // 부분합의 길이를 1부터 len까지 처리
+    // 부분합 계산
     for (int subLen = 1; subLen <= len; subLen++) {
         for (int start = 0; start < len; start++) {
-            // 슬라이딩 윈도우를 이용하여 부분합 계산
-            int sum = 0;
-            for (int i = start; i < start + subLen; i++) {
-                sum += extendedElements[i];
-            }
+            int sum = prefixSum[start + subLen] - prefixSum[start];
             sumOfSub.insert(sum);
         }
     }
