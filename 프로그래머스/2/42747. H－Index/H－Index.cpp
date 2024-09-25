0876a845f3c634cca72a90ba3147a8e778b5d59c
nastorond <86664097+nastorond@users.noreply.github.com>
2024-09-25 11:40:21 +0900

    [level 2] Title: H-Index, Time: 0.08 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,18 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0, len=citations.size();
-    sort(citations.begin(), citations.end());
-    
-    for (int i=0; i<len; i++) {
-        if (citations[i] >= len-i) {
-            return len-i;
-        }
-    }
-    
+    int answer = 0;
     return answer;
 }
